43b6cd9e71d4eabfc8cc178ea9981e05151a6867
Dmitry.Vlasov <dmitry.vlasov@softlex.pro>
2021-05-06 19:14:23 +0700

    yandex WIP#2

--- src/components/authentication/register/hooks/useOAuthRegister.ts
+++ src/components/authentication/register/hooks/useOAuthRegister.ts
@@ -1,31 +1,28 @@
 import { OAuthConfig } from '../../../../config';
-import { authAPI } from '../../../../api/authAPI';
 
 export const useOAuthRegister = () => {
   const googleOAuth = (response) => {
     console.log(response);
   };
 
-  const { getCodeYandexOAuth } = authAPI();
-
   const yandexOAuth = () => {
-    // fetch(`https://oauth.yandex.ru/authorize?response_type=code&client_id=${OAuthConfig.secretID}`, {
-    //   mode: 'no-cors',
-    //   method: 'GET',
-    //   redirect: 'follow',
-    // }).then((res) => {
-    //   console.log(res);
-    // }).catch((err) => {
-    //   console.log(err);
-    // });
-    getCodeYandexOAuth({
-      payload: {
-        client_id: OAuthConfig.secretID,
-        response_type: 'code',
-      },
-      onError: (errorResponse) => console.log(errorResponse),
-      onSuccess: (res) => console.log(res)
+    fetch(`https://oauth.yandex.ru/authorize?response_type=code&client_id=${OAuthConfig.secretID}`, {
+      mode: 'no-cors',
+      method: 'GET',
+      redirect: 'follow',
+    }).then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.log(err);
     });
+    // getCodeYandexOAuth({
+    //   payload: {
+    //     client_id: OAuthConfig.secretID,
+    //     response_type: 'code',
+    //   },
+    //   onError: (errorResponse) => console.log(errorResponse),
+    //   onSuccess: (res) => console.log(res)
+    // });
   };
 
   return { googleOAuth, yandexOAuth };
