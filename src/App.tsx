658138331c4d3ece0a29908ac327e7d685fd20c1
Dmitry.Vlasov <dmitry.vlasov@softlex.pro>
2021-05-06 12:23:21 +0700

    Created sprint1 branch

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App: FC = () => {
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
   } else {
-    firebase.app(); // if already initialized, use that one
+    firebase.app();
   }
 
   const content = useRoutes(routes);
