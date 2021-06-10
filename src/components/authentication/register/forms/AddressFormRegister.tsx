916cdaea6e83066b860728e783ca626d3bc59893
Yan.Doshinskiy <yan.doshinskiy@softlex.pro>
2021-06-10 17:13:51 +0700

    registration 1 page fields changed

--- src/components/authentication/register/forms/AddressFormRegister.tsx
+++ src/components/authentication/register/forms/AddressFormRegister.tsx
@@ -1,7 +1,16 @@
 import type { FC } from 'react';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
-import { Autocomplete, Box, Button, TextField, Typography, Checkbox, FormControlLabel } from '@material-ui/core';
+import {
+  Autocomplete,
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Checkbox,
+  FormControlLabel,
+  TextareaAutosize,
+} from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import useIsMountedRef from '../../../../hooks/useIsMountedRef';
 import { useFetchAddresses } from '../hooks/useFetchAddresses';
@@ -15,7 +24,7 @@ const AddressFormRegister: FC = (props) => {
   const { fetchAddresses, addresses: options, countries, fetchCounties } = useFetchAddresses();
   const { setRegisterStep } = authActionCreators();
   const { check, status, error } = useCheckAddress(setRegisterStep);
-  const [withCountry, setWithCountry] = useState(false);
+  const [withCountry, setWithCountry] = useState(true);
 
   useEffect(() => {
     fetchCounties();
@@ -23,43 +32,29 @@ const AddressFormRegister: FC = (props) => {
 
   return (
     <>
-      <Typography
-        color="#747373"
-        gutterBottom
-      >
+      <Typography color="#747373" gutterBottom>
         Где вы имеете право голоса?
       </Typography>
       <Box
         sx={{
           flexGrow: 1,
-          mt: 3
+          mt: 3,
         }}
       >
         <Formik
           initialValues={{
             address: '',
             country_id: '',
-            submit: null
+            submit: null,
           }}
-          validationSchema={
-          Yup
-            .object()
-            .shape({
-              address: Yup
-                .string(),
-              country_id: Yup
-                .string()
-            })
-        }
-          onSubmit={async (values, {
-            setErrors,
-            setStatus,
-            setSubmitting
-          }): Promise<void> => {
+          validationSchema={Yup.object().shape({
+            address: Yup.string(),
+            country_id: Yup.string(),
+          })}
+          onSubmit={async (values, { setErrors, setStatus, setSubmitting }): Promise<void> => {
             try {
               await check(values.address, values.country_id, withCountry, countries);
             } catch (err) {
-              console.error(err);
               if (isMountedRef.current) {
                 setStatus({ success: false });
                 setErrors({ submit: err.message });
@@ -68,91 +63,72 @@ const AddressFormRegister: FC = (props) => {
             }
           }}
         >
-          {({
-            errors,
-            handleBlur,
-            handleChange,
-            handleSubmit,
-            touched,
-            values
-          }): JSX.Element => (
-            <form
-              noValidate
-              onSubmit={handleSubmit}
-              {...props}
-            >
-              <Autocomplete
-                disabled={withCountry}
-                fullWidth
-                options={options || []}
-                noOptionsText={<>Нет доступных вариантов</>}
-                onInputChange={(value, newValue) => {
-                  fetchAddresses(newValue);
-                  handleChange(newValue);
-                }}
-                renderInput={(params) => (
-                  <TextField
-                    disabled={withCountry}
-                    {...params}
-                    helperText={(touched.address && errors.address) || error}
-                    error={(touched.address && !!errors.address) || !!error}
-                    label="Начните вводить город или улицу"
-                    margin="normal"
-                    name="address"
-                    variant="outlined"
-                    onSelect={(value) => {
-                      // @ts-ignore
-                      fetchAddresses(value.target.defaultValue);
-                      handleChange(value);
-                    }}
-                    value={values.address}
-                  />
-                )}
-                onBlur={handleBlur}
-                value={values.address}
-              />
-              <Box sx={{ mt: 1, ml: 2 }}>
-                <FormControlLabel
-                  control={(
-                    <Checkbox
-                      checked={withCountry}
-                      onChange={(e) => setWithCountry(e.target.checked)}
-                      color="primary"
-                      name="checkedC"
+          {({ errors, handleBlur, handleChange, handleSubmit, touched, values }): JSX.Element => (
+            <form noValidate onSubmit={handleSubmit} {...props}>
+              <Box sx={{ mt: 0.5 }} style={{ marginBottom: !withCountry ? '20px' : '0px' }}>
+                <Autocomplete
+                  fullWidth
+                  options={countries.map((item) => item.title) || []}
+                  noOptionsText={<>Нет доступных вариантов</>}
+                  onInputChange={(value, newValue) => {
+                    if (newValue.toLowerCase() === 'россия' || newValue.toLowerCase() === 'российская федерация') {
+                      setWithCountry(false);
+                    } else {
+                      setWithCountry(true);
+                    }
+                    handleChange(newValue);
+                  }}
+                  limitTags={15}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Начните вводить страну"
+                      margin="normal"
+                      name="country_id"
+                      variant="outlined"
+                      onSelect={(value) => {
+                        handleChange(value);
+                      }}
+                      value={values.country_id}
                     />
-                      )}
-                  label="Я не из России"
+                  )}
+                  onBlur={handleBlur}
+                  value={values.country_id}
                 />
               </Box>
-              {withCountry && (
-                <Box sx={{ mt: 0.5 }}>
-                  <Autocomplete
-                    fullWidth
-                    options={countries.map((item) => item.title) || []}
-                    noOptionsText={<>Нет доступных вариантов</>}
-                    onInputChange={(value, newValue) => {
-                      handleChange(newValue);
-                    }}
-                    limitTags={15}
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        label="Начните вводить страну"
-                        margin="normal"
-                        name="country_id"
-                        variant="outlined"
-                        onSelect={(value) => {
-                          console.log(value);
-                          handleChange(value);
-                        }}
-                        value={values.country_id}
-                      />
-                    )}
-                    onBlur={handleBlur}
-                    value={values.country_id}
-                  />
-                </Box>
+              {!withCountry && (
+                <Autocomplete
+                  disabled={withCountry}
+                  fullWidth
+                  options={options || []}
+                  noOptionsText={<>Нет доступных вариантов</>}
+                  onInputChange={(value, newValue) => {
+                    fetchAddresses(newValue);
+                    handleChange(newValue);
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      disabled={withCountry}
+                      {...params}
+                      helperText={(touched.address && errors.address) || error}
+                      error={(touched.address && !!errors.address) || !!error}
+                      label="Начните вводить город или улицу"
+                      margin="normal"
+                      name="address"
+                      variant="outlined"
+                      onSelect={(value) => {
+                        // @ts-ignore
+                        fetchAddresses(value.target.defaultValue);
+                        handleChange(value);
+                      }}
+                      value={values.address}
+                    />
+                  )}
+                  onBlur={handleBlur}
+                  value={values.address}
+                />
               )}
+
               <Box sx={{ mt: 2 }}>
                 <Button
                   color="primary"
