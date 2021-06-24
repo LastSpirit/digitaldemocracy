0888942e805777a382ed2f61571f53e791499f54
Yan.Doshinskiy <yan.doshinskiy@softlex.pro>
2021-06-24 23:06:19 +0700

    objectpage mobile cards layout added

--- src/pages/SingleNewsPage/features/SingleNewsStatistics/StatisticsCard.tsx
+++ src/pages/SingleNewsPage/features/SingleNewsStatistics/StatisticsCard.tsx
@@ -4,6 +4,7 @@ import { Box, IconButton, Card, Typography } from '@material-ui/core';
 import { avatarColorChanger } from 'src/utils/avatarColorChanger';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import { useWindowSize } from 'src/hooks/useWindowSize';
 import { MassmediaVotesGroup } from '../VotesGroup/MassmediaVotesGroup';
 import { AuthorVotesGroup } from '../VotesGroup/AuthorVotesGroup';
 import { PoliticianVotesGroup } from '../VotesGroup/PoliticianVotesGroup';
@@ -41,49 +42,128 @@ const StatisticsCard: FC<StatisticsCardPropsI> = ({
   isMasmedia,
 }) => {
   const percentIsPositive = percent?.includes('+') && !percent?.includes('-');
+  const { isMobile } = useWindowSize();
   return (
-    <div className={styles.card}>
-      <Link to={`${field}/${short_link}`} className={styles.image}>
-        <img src={avatarColorChanger(rating)} alt="frame" className={styles.frame} />
-        <div className={styles.photoContainer}>
-          <img src={photo} alt="avatar" className={isMasmedia ? styles.massmediaPhoto : styles.photo} />
-        </div>
-      </Link>
-      <div className={styles.cardContent}>
-        <div className={styles.title}>{name}</div>
-
-        <div className={styles.bottomItem}>
-          {percent ? (
-            <div className={styles.percent}>
-              {percentIsPositive ? (
-                <ArrowUpwardIcon className={styles.upIcon} />
-              ) : (
-                <ArrowDownwardIcon className={styles.downIcon} />
-              )}
-              <div className={styles.text}>{percent}</div>
+    <>
+      {isMobile ? (
+        <div className={styles['card-mobile']}>
+          <div className={styles.topItems}>
+            <Link to={`${field}/${short_link}`} className={styles.image}>
+              <img src={avatarColorChanger(rating)} alt="frame" className={styles.frame} />
+              <div className={styles.photoContainer}>
+                <img src={photo} alt="avatar" className={isMasmedia ? styles.massmediaPhoto : styles.photo} />
+              </div>
+            </Link>
+            <div className={styles.cardContent}>
+              <div className={styles.title}>{name}</div>
+              <div className={styles.percent}>
+                {percentIsPositive ? (
+                  <ArrowUpwardIcon className={styles.upIcon} viewBox="3 0 24 24" />
+                ) : (
+                  <ArrowDownwardIcon className={styles.downIcon} viewBox="3 0 24 24" />
+                )}
+                <div className={styles.text}>{percent}</div>
+              </div>
             </div>
-          ) : (
-            <></>
-          )}
-          {field === '/mass-media' ? (
-            <MassmediaVotesGroup likes={likes} dislikes={dislikes} isLiked={isLiked} isDisliked={isDisliked} />
-          ) : field === '/author' ? (
-            <AuthorVotesGroup likes={likes} dislikes={dislikes} isLiked={isLiked} isDisliked={isDisliked} />
-          ) : field === '/politician' ? (
-            <PoliticianVotesGroup
-              likes={likes}
-              dislikes={dislikes}
-              isLiked={isLiked}
-              isDisliked={isDisliked}
-              index={politicianIndex}
-              id={id}
-            />
-          ) : (
-            <></>
-          )}
+          </div>
+          <div className={styles.bottomItems}>
+            {field === '/mass-media' ? (
+              <MassmediaVotesGroup likes={likes} dislikes={dislikes} isLiked={isLiked} isDisliked={isDisliked} />
+            ) : field === '/author' ? (
+              <AuthorVotesGroup likes={likes} dislikes={dislikes} isLiked={isLiked} isDisliked={isDisliked} />
+            ) : field === '/politician' ? (
+              <PoliticianVotesGroup
+                likes={likes}
+                dislikes={dislikes}
+                isLiked={isLiked}
+                isDisliked={isDisliked}
+                index={politicianIndex}
+                id={id}
+              />
+            ) : (
+              <></>
+            )}
+          </div>
+
+          {/* <div className={styles.cardContent}>
+            <div className={styles.title}>{name}</div>
+            <div className={styles.bottomItem}>
+              {percent ? (
+                <div className={styles.percent}>
+                  {percentIsPositive ? (
+                    <ArrowUpwardIcon className={styles.upIcon} />
+                  ) : (
+                    <ArrowDownwardIcon className={styles.downIcon} />
+                  )}
+                  <div className={styles.text}>{percent}</div>
+                </div>
+              ) : (
+                <></>
+              )}
+              {field === '/mass-media' ? (
+                <MassmediaVotesGroup likes={likes} dislikes={dislikes} isLiked={isLiked} isDisliked={isDisliked} />
+              ) : field === '/author' ? (
+                <AuthorVotesGroup likes={likes} dislikes={dislikes} isLiked={isLiked} isDisliked={isDisliked} />
+              ) : field === '/politician' ? (
+                <PoliticianVotesGroup
+                  likes={likes}
+                  dislikes={dislikes}
+                  isLiked={isLiked}
+                  isDisliked={isDisliked}
+                  index={politicianIndex}
+                  id={id}
+                />
+              ) : (
+                <></>
+              )}
+            </div>
+          </div> */}
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className={styles.card}>
+          <Link to={`${field}/${short_link}`} className={styles.image}>
+            <img src={avatarColorChanger(rating)} alt="frame" className={styles.frame} />
+            <div className={styles.photoContainer}>
+              <img src={photo} alt="avatar" className={isMasmedia ? styles.massmediaPhoto : styles.photo} />
+            </div>
+          </Link>
+          <div className={styles.cardContent}>
+            <div className={styles.title}>{name}</div>
+
+            <div className={styles.bottomItem}>
+              {percent ? (
+                <div className={styles.percent}>
+                  {percentIsPositive ? (
+                    <ArrowUpwardIcon className={styles.upIcon} />
+                  ) : (
+                    <ArrowDownwardIcon className={styles.downIcon} />
+                  )}
+                  <div className={styles.text}>{percent}</div>
+                </div>
+              ) : (
+                <></>
+              )}
+              {field === '/mass-media' ? (
+                <MassmediaVotesGroup likes={likes} dislikes={dislikes} isLiked={isLiked} isDisliked={isDisliked} />
+              ) : field === '/author' ? (
+                <AuthorVotesGroup likes={likes} dislikes={dislikes} isLiked={isLiked} isDisliked={isDisliked} />
+              ) : field === '/politician' ? (
+                <PoliticianVotesGroup
+                  likes={likes}
+                  dislikes={dislikes}
+                  isLiked={isLiked}
+                  isDisliked={isDisliked}
+                  index={politicianIndex}
+                  id={id}
+                />
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default StatisticsCard;
