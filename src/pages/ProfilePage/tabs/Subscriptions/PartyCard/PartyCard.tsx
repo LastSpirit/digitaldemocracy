72d0faa27aa9f62aac2e7277fe84278b34c7f2dd
Evgeny.Burakov <evgeny.burakov@softlex.pro>
2021-07-07 19:03:09 +0300

    Add politicians sub

---
+++ src/pages/ProfilePage/tabs/Subscriptions/PartyCard/PartyCard.tsx
@@ -0,0 +1,82 @@
+import React, { FC } from 'react';
+import PersonIcon from '@material-ui/icons/Person';
+import { useHistory, matchPath } from 'react-router';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { useSelector } from 'react-redux';
+import { Button, Tooltip } from '@material-ui/core';
+import { badgeColorChanger } from 'src/utils/badgeColorChanger';
+import { avatarColorChanger } from 'src/utils/avatarColorChanger';
+import styles from './PartyCard.module.scss';
+import { useSearchParams } from '../../../../../hooks/useSearchParams';
+import { ModalParams } from '../../../../../types/routing';
+import { userSelectors } from '../../../../../slices/userSlice';
+import { PoliticianInfoI } from '../../../../../slices/politicianSlice';
+import { Loading } from '../../../../../components/Loading/Loading';
+import { useChangeSubscribe } from '../../../../PartyPage/hooks/useChangeSubscribe';
+import { APIStatus } from '../../../../../lib/axiosAPI';
+
+interface IProps extends PoliticianInfoI {}
+
+const PartyCard: FC<IProps> = ({ photo, rating, name, is_subscribed, id, short_link, position, place }) => {
+  const isAuthenticated = useSelector(userSelectors.getIsAuthenticated());
+  const { status, change } = useChangeSubscribe(id);
+  const { push } = useHistory();
+  const {
+    [ModalParams.Auth]: { setValue: setAuthValue },
+  } = useSearchParams(ModalParams.Auth);
+  const handleClick = () => {
+    if (!isAuthenticated) {
+      setAuthValue('/login');
+    }
+  };
+
+  return (
+    <div className={styles.root}>
+      <Link to={`/politician/${short_link}/politician_news`}>
+        <div
+          className={styles.avatarBlock}
+          style={{ backgroundImage: `url(${avatarColorChanger(rating)})`, backgroundSize: 'cover' }}
+        >
+          <div className={styles.avatar}>
+            {!photo ? <PersonIcon className={styles.noAvatarIcon} /> : <img src={photo} alt="" />}
+          </div>
+        </div>
+      </Link>
+      <div className={styles.second}>
+        <div
+          className={styles.badge}
+          style={{
+            backgroundColor: badgeColorChanger(rating),
+          }}
+        >
+          <div className={styles.text}>{`Место ${place ?? '-'}`}</div>
+        </div>
+        <div className={styles.percent}>{rating ?? '-'} %</div>
+      </div>
+      <hr />
+      <div className={styles.name}>{name}</div>
+      <div className={styles.position}>{position}</div>
+      <Button
+        variant="outlined"
+        color={is_subscribed ? 'secondary' : 'primary'}
+        onClick={isAuthenticated ? change : handleClick}
+        disabled={status === APIStatus.Loading}
+        className={classNames([
+          'MuiButton-containedPrimary',
+          styles.subscriberButton,
+          { '-disabled': !isAuthenticated },
+        ])}
+      >
+        <Tooltip title={isAuthenticated ? '' : 'Вы не авторизованы'}>
+          <span>
+            {/* eslint-disable-next-line no-nested-ternary */}
+            {status === APIStatus.Loading ? <Loading /> : is_subscribed ? 'Отписаться' : 'Следить'}
+          </span>
+        </Tooltip>
+      </Button>
+    </div>
+  );
+};
+
+export default PartyCard;
--- src/pages/ProfilePage/tabs/Subscriptions/Subscriptions.tsx
+++ src/pages/ProfilePage/tabs/Subscriptions/Subscriptions.tsx
@@ -1,6 +1,68 @@
-import React from 'react';
-import InDevelop from '../../../../components/InDevelop/InDevelop';
+import React, { useReducer, useEffect } from 'react';
+import { Button } from '@material-ui/core';
+// import InDevelop from '../../../../components/InDevelop/InDevelop';
+import { useSelector } from 'react-redux';
+import PartyCard from './PartyCard/PartyCard';
+import { useFetchPartyPoliticians } from '../../../PartyPage/hooks/useFetchPoliticians';
+import classes from './Subscriptions.module.scss';
 
-export const Subscriptions = () => (
-  <InDevelop />
-);
+import { partySelectors } from '../../../../slices/partySlice';
+import { userSelectors } from '../../../../slices/userSlice';
+
+interface IState {
+  id: number;
+  title: string;
+  active: boolean;
+}
+
+const initialState: IState[] = [
+  { id: 1, title: 'Политики', active: true },
+  { id: 2, title: 'CМИ', active: false },
+  { id: 3, title: 'Авторы', active: false },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setActive':
+      return [...state].map((item) => (item.id === action.id ? { ...item, active: true } : { ...item, active: false }));
+    default:
+      return state;
+  }
+}
+
+export const Subscriptions = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const data = useSelector(partySelectors.getPartyPoliticians());
+  const { fetch, status } = useFetchPartyPoliticians();
+  const isAuthenticated = useSelector(userSelectors.getIsAuthenticated());
+
+  useEffect(() => {
+    fetch(1);
+  }, [isAuthenticated]);
+
+  const setCards = () => {
+    return state.find((item) => item.active === true).title;
+  };
+  return (
+    <div className={classes.root}>
+      <div className={classes.buttons}>
+        {state.map(({ id, title, active }) => {
+          return (
+            <Button
+              key={id}
+              className={active ? classes.buttonStyle : classes.buttonStyleActive}
+              onClick={() => dispatch({ type: 'setActive', id })}
+            >
+              {title}
+            </Button>
+          );
+        })}
+      </div>
+      <div className={classes.party}>
+        {data?.politicians?.map((item, index) => (
+          <PartyCard key={item.id} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
